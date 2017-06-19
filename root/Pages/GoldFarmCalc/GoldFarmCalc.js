bf81396036a5584d5c2913ba4d6dca440d6b03cd
ShinyMeta <smetagross@gmail.com>
2017-06-19 09:48:05 -0600

    Able to add, delete and edit method names and affect the database.

--- root/Pages/GoldFarmCalc/GoldFarmCalc.js
+++ root/Pages/GoldFarmCalc/GoldFarmCalc.js
@@ -8,6 +8,8 @@ var methodSelect = document.getElementById("methodSelect");
 
 var startButton = document.getElementById('startButton');
 var endButton = document.getElementById('endButton');
+var deleteButton = document.getElementById('deleteButton');
+var editButton = document.getElementById('editButton');
 
 
 ///////////////////////////////////////////
@@ -61,6 +63,18 @@ function startButtonListener(e) {
     //if default, then do nothing
   } else if (methodSelect.value == 'add'){
     //if add new, then prompt for name and send server requeest to make new farm
+    var name = prompt ("What is the name of this method?");
+    var method = {name: name, username: username};
+
+    postHttpRequest('/goldFarm/newmethod', method, function(response){
+      var wasAdded = JSON.parse(response);
+      if (wasAdded){
+        addMethodToSelect(name, methodSelect.length-1);
+      }
+      else {
+        alert ('There was an error adding the method to the server.  Please try again.');
+      }
+    });
   } else {
     //otherwise, send start run request and begin timer and stuff
 
@@ -71,9 +85,45 @@ function startButtonListener(e) {
 
 
 
+deleteButton.addEventListener('click', deleteButtonListener);
+
+function deleteButtonListener(e){
+
+  //if default or add button is selected, alert to select a method
+  if (methodSelect.value == 'default' || methodSelect.value == 'add'){
+    alert('You have not selected a method to delete.  Please select a method first.');
+  }
+  //if a method is selected, double confirm that all data will be lost.
+  else {
+    var areTheySureTheyWantToDelete = confirm('Are you sure you want to delete the method ' + methodSelect.value +
+      ' from the server?  All data relating to this method will be lost.');
+    if (areTheySureTheyWantToDelete){
+      //make a delete request
+      makeDeleteRequest();
+    }
+  }
+}
 
 
 
+editButton.addEventListener('click', editButtonListener);
+
+function editButtonListener(e){
+  //if default or add button is selected, alert to select a method
+  if (methodSelect.value == 'default' || methodSelect.value == 'add'){
+    alert('You have not selected a method to edit.  Please select a method first.');
+  }
+  //if a method is selected, prompt for new name.
+  else {
+    var newName = prompt("What would you like the new name for '" +
+      methodSelect.value + '" to be?:');
+
+    //make an edit request to rename the method
+    if (newName != methodSelect.value){
+      makeEditRequest(newName);
+    }
+  }
+}
 
 
 
@@ -97,19 +147,76 @@ function getHttpRequest(URL, responseFunction){
 }
 
 
+//generic "POST" sending a JSON encoded object
+function postHttpRequest(URL, object, responseFunction){
+  var xmlhttp = new XMLHttpRequest();
+  xmlhttp.open("POST", URL, true);
+  xmlhttp.setRequestHeader("Content-Type", "application/json");
+
+  xmlhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      //response received, call response function now
+      responseFunction(this.response);
+    }
+  };
+  xmlhttp.send(JSON.stringify(object));
+}
 
 
 
+
+function makeEditRequest(newName){
+  var methodToEdit = {name: methodSelect.value, newName: newName, username: username};
+  var indexToEdit = methodSelect.selectedIndex;
+
+  postHttpRequest('/goldFarm/editmethod', methodToEdit, function(response){
+    var wasEdited = JSON.parse(response);
+    if (wasEdited){
+      //need to update the select by deleting and adding again
+      deleteMethodFromSelect(indexToEdit);
+      addMethodToSelect(newName, indexToEdit);
+    }
+    else {
+      alert('There was an error editing the method on the server. Please try again.');
+    }
+  });
+}
+
+
+
+function makeDeleteRequest() {
+  var methodToDelete = {name: methodSelect.value, username: username};
+  var methodIndex = methodSelect.selectedIndex;
+
+  postHttpRequest('/goldFarm/deletemethod', methodToDelete, function(response){
+    var wasDeleted = JSON.parse(response);
+    if (wasDeleted){
+      //alert ('The method was successfully deleted');
+      deleteMethodFromSelect(methodIndex);
+    }
+    else {
+      alert('There was an error deleting the method from the server. Please try again.');
+    }
+  });
+}
 
 function receiveMethods(response) {
   var methods = JSON.parse(response);
   if (methods.length !== 0){
     //set the selector to include all of the methods
     for (var i = 0; i < methods.length; i++){
-      var newOption = document.createElement('option');
-      newOption.value = methods[i].name;
-      newOption.innerHTML = methods[i].name;
-      methodSelect.add(newOption, i+1);
+      addMethodToSelect(methods[i].name, i+1);
     }
   }
+}
+
+function addMethodToSelect(name, index){
+  var newOption = document.createElement('option');
+  newOption.value = name;
+  newOption.innerHTML = name;
+  methodSelect.add(newOption, index);
+}
+
+function deleteMethodFromSelect(index){
+  methodSelect.remove(index);
 }
--- routes/goldFarm.js
+++ routes/goldFarm.js
@@ -4,8 +4,11 @@ var router = express.Router();
 var mysql = require('mysql');
 
 
-//module.exports = router;
 module.exports = router;
+
+//module.exports = addMethod;
+
+
 
 var API_KEY = 'access_token=58468D10-F7BA-234C-963E-A854D0001EC150A288B8-F8F2-4611-A664-C526CE446B20';
 
@@ -16,14 +19,13 @@ var matStorageRequestURL = 'api.guildwars2.com/v2/account/materials?access_token
 
 
 
-var mysqlConnection = mysql.createConnection({
+var pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
   password: 'mywebserver',
   database: 'goldfarmingdatabase'
 });
 
-mysqlConnection.connect();
 
 
 
@@ -36,9 +38,29 @@ router
       res.send(result);
     });
   })
-  .post('/', function (req, res) {
-
-
+  .post('/newmethod', function (req, res) {
+    //console.log(req.body);
+    //add method/return false if method exists
+    addMethod(req.body.name, req.body.username, function(result){
+      console.log (result);
+      res.send(result);
+    });
+  })
+  .post('/deletemethod', function (req, res) {
+    //console.log(req.body);
+    //delete method/return false if method exists
+    deleteMethod(req.body.name, req.body.username, function(result){
+      console.log(result);
+      res.send(result);
+    });
+  })
+  .post('/editmethod', function (req, res) {
+    //console.log(req.body);
+    //delete method/return false if method exists
+    editMethod(req.body.name, req.body.newName, req.body.username, function(result){
+      console.log(result);
+      res.send(result);
+    });
   })
 ;
 
@@ -56,16 +78,194 @@ router
 //search mySQL database for methods under username
 function getMethodsByUsername(username, callback){
   //sql query for table with only methods with a userid that matches the username
-  var queryString = 'SELECT name FROM users INNER JOIN methods ON users.id = methods.userid ' +
-    'WHERE users.username = ' + mysqlConnection.escape(username);
 
-  var query = mysqlConnection.query(queryString, function(err, result){
-    console.log(query.sql);
-
+  pool.getConnection(function(err, mysqlConnection){
     if (err){
       console.error(err);
       return;
     }
-    callback(result);
+    var queryString = 'SELECT name FROM users INNER JOIN methods ON users.id = methods.userid ' +
+      'WHERE users.username = ' + mysqlConnection.escape(username);
+    var query = mysqlConnection.query(queryString, function(err, result){
+      //console.log(query.sql);
+      mysqlConnection.release();
+
+      if (err){
+        console.error(err);
+        return;
+      }
+      callback(result);
+    });
+  });
+}
+
+//add method to methods table, return success status
+function addMethod(methodName, username, callback){
+
+  //first verify that username is correct
+  getUserIdByUsername(username, function(userid){
+    //next check if method with same user and name exists, if so, return false (unsuccessful)
+    checkMethodNameExists(methodName, userid, function(methodExists){
+
+      if(methodExists){
+        callback(false);
+      }
+      else {
+        pool.getConnection(function(err, mysqlConnection){
+          if (err){
+            console.error(err);
+            return;
+          }
+          //here, we know method does not exist, so we can add safely
+          var queryString = 'INSERT INTO methods (name, userid) VALUES ' +
+            '("' + methodName + '",' + userid + ')';
+          var query = mysqlConnection.query(queryString, function(err, result){
+            //console.log(query.sql);
+            mysqlConnection.release();
+            if (err){
+              console.error(err);
+              return;
+            }
+            // console.log(result);
+            //if you made it this far, we successfully added the method
+            callback(true);
+          });
+        });
+      }
+    });
+  });
+
+
+
+
+}
+
+//delete method return succes status
+function deleteMethod(methodName, username, callback){
+  //first verify that username is correct
+  getUserIdByUsername(username, function(userid){
+    //next check if method with same user and name exists, if not, return false (unsuccessful)
+    checkMethodNameExists(methodName, userid, function(methodExists){
+
+      if(!methodExists){
+        callback(false);
+      }
+      else {
+        //here, we know method does exist, so we can delete safely
+        pool.getConnection(function(err, mysqlConnection){
+          if (err){
+            console.error(err);
+            return;
+          }
+          var queryString = 'DELETE FROM methods WHERE name = "' + methodName + '"';
+          var query = mysqlConnection.query(queryString, function(err, result){
+            //console.log(query.sql);
+            mysqlConnection.release();
+            if (err){
+              console.error(err);
+              callback(false);
+              return;
+            }
+            // console.log(result);
+            //if you made it this far, we successfully deleted the method
+            callback(true);
+          });
+        });
+      }
+    });
+  });
+
+}
+
+//edit method name return succes status
+function editMethod(methodName, newName, username, callback){
+  //first verify that username is correct
+  getUserIdByUsername(username, function(userid){
+    //next check if method with same user and name exists, if not, return false (unsuccessful)
+    checkMethodNameExists(methodName, userid, function(methodExists){
+
+      if(!methodExists){
+        callback(false);
+      }
+      else {
+        //here, we know method does exist, so we can edit safely
+        pool.getConnection(function(err, mysqlConnection){
+          if (err){
+            console.error(err);
+            return;
+          }
+          var queryString = 'UPDATE methods SET name = "' + newName +
+            '" WHERE name = "' + methodName + '"';
+          var query = mysqlConnection.query(queryString, function(err, result){
+            //console.log(query.sql);
+            mysqlConnection.release();
+            if (err){
+              console.error(err);
+              callback(false);
+              return;
+            }
+            // console.log(result);
+            //if you made it this far, we successfully edited the method
+            callback(true);
+          });
+        });
+      }
+    });
+  });
+
+}
+
+function checkMethodNameExists(name, userid, callback){
+  //check if method with same user and name exists, if so, return false (unsuccessful)
+  pool.getConnection(function(err, mysqlConnection){
+    if (err){
+      console.error(err);
+      return;
+    }
+    var queryString = 'SELECT id FROM methods ' +
+      'WHERE name = "' + name + '" AND userid = ' + userid;
+
+    var query = mysqlConnection.query(queryString, function(err, result){
+      // console.log(query.sql);
+      mysqlConnection.release();
+      if (err){
+        console.error(err);
+        return;
+      }
+      // console.log(result);
+      if (result.length === 0){
+        //if no results, that means there is no method with same name for this userid
+        callback(false);
+      }
+      else {
+        callback(true);
+      }
+    });
+  });
+}
+
+function getUserIdByUsername(username, callback){
+  pool.getConnection(function(err, mysqlConnection){
+    if (err){
+      console.error(err);
+      return;
+    }
+    var queryString = "SELECT id FROM users WHERE username = '" + username + "'";
+    var query = mysqlConnection.query(queryString, function(err, result){
+      // console.log(query.sql);
+      mysqlConnection.release();
+      if (err){
+        console.error(err);
+        return;
+      }
+      // console.log(result);
+      //if there is anything in the list, return the user, otherwise return 0
+      if (result.length === 1){
+        callback(result[0].id);
+      }
+      else {
+        callback(0);
+      }
+    });
   });
 }
