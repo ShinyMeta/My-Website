08d1d131f9303e998ddf058475987a34446eaa39
shinymeta <zach.swalberg@gmail.com>
2025-06-05 15:37:53 -0400

    testing greenlock

--- ExpressServer_dev.js
+++ ExpressServer_dev.js
@@ -1,143 +1,108 @@
-
 //ExpressServer_dev is the source, and gets babel-node built into ExpressServer for production
 
-
-
-const bodyParser = require('body-parser')
-const express = require('express')
-const fs = require('fs')
-const helmet = require('helmet')
-const http = require('http')
-const https = require('https')
-const path = require('path')
-import webpack from 'webpack'
+const bodyParser = require('body-parser');
+const express = require('express');
+const fs = require('fs');
+const helmet = require('helmet');
+const http = require('http');
+// const https = require('https');
+const path = require('path');
+import webpack from 'webpack';
 // import webpackMiddleware from 'webpack-dev-middleware'
 // import webpackHot from 'webpack-hot-middleware'
-import webpackConfig from './webpack.dev.config.js'
-
-
-
-
+import webpackConfig from './webpack.dev.config.js';
 
 //app is for controlling the server
-let app = express()
-let httpServer = http.createServer(app)
+let app = express();
+let httpServer = http.createServer(app);
 
-const privateKey = fs.readFileSync('C:/SSL/private.key')
-const certificate = fs.readFileSync('C:/SSL/certificate.crt')
-const credentials = {key: privateKey, cert: certificate}
-let httpsServer = https.createServer(credentials, app)
-
-
-
-
+// const privateKey = fs.readFileSync('C:/SSL/private.key');
+// const certificate = fs.readFileSync('C:/SSL/certificate.crt');
+// const credentials = { key: privateKey, cert: certificate };
+// let httpsServer = https.createServer(credentials, app)
 
 ///////////////////////////////////////////////
 //  PLACE TO RUN SCRIPT ON SERVER START
 ///////////////////////////////////////////////
 
-
-
-
-
 //////////////////////////////////////////////
 //////////////////////////////////////////////
 //////////////////////////////////////////////
-
-
-
-
-
-
-
-
-
 
 //http requests with content type application/json will be parsed
 //req.body will be changed to a json object
-app.use(bodyParser.json({limit: '50mb'}))
-  .use(bodyParser.urlencoded({limit: '50mb', extended: true}))
+app
+  .use(bodyParser.json({ limit: '50mb' }))
+  .use(bodyParser.urlencoded({ limit: '50mb', extended: true }))
 
-// tells the browser to use https if they are on http
+  // tells the browser to use https if they are on http
   .use(helmet())
 
-
-
-  .set('view engine', 'hjs')
-
+  .set('view engine', 'hjs');
 
 //router for gw2daata JSON responses
-app.use('/gw2data', require('./server_routes/gw2data_route'))
-
+app.use('/gw2data', require('./server_routes/gw2data_route'));
 
 if (process.env.NODE_ENV === 'development') {
-  const webpackDevMiddleware = require('webpack-dev-middleware')(webpack(webpackConfig), {
-    publicPath: webpackConfig.output.publicPath
-  })
+  const webpackDevMiddleware = require('webpack-dev-middleware')(
+    webpack(webpackConfig),
+    {
+      publicPath: webpackConfig.output.publicPath,
+    }
+  );
 
-  app.use(webpackDevMiddleware)
+  app.use(webpackDevMiddleware);
 
   app.use(require('webpack-hot-middleware')(webpack(webpackConfig)));
 
   app.get('/gw2data/*', (req, res, next) => {
     const indexHTML = webpackDevMiddleware.fileSystem.readFileSync(
-      require('path').join(webpackConfig.output.path, 'index.html'), 'utf-8')
-    res.send(indexHTML)
-  })
+      require('path').join(webpackConfig.output.path, 'index.html'),
+      'utf-8'
+    );
+    res.send(indexHTML);
+  });
 }
 
-
 //Easy routing for requesting html and images, etc.
-app.use(express.static('website_public'))
-
+app.use(express.static('website_public'));
 
 //if request reaches this, then physical file doesn't exist
-app.get('/gw2data/*', (req, res, next) => {
-  res.sendFile(path.resolve(__dirname,'./website_public/gw2data/index.html'))
-})
+app
+  .get('/gw2data/*', (req, res, next) => {
+    res.sendFile(
+      path.resolve(__dirname, './website_public/gw2data/index.html')
+    );
+  })
 
+  //for that asshole that's just trying to POST over and over again
+  // app.post('/', (req, res, next) => {
+  //   console.log (`that asshole that keeps POSTing did it again... IP: ${req.ip}`)
+  //   res.status(403).send('fuck you')
+  // })
 
+  ///////////////////////////////////
+  //    ERROR RESPONSES
+  ///////////////////////////////////
 
+  .use(function routeNotFound(req, res, next) {
+    let message =
+      `Not Found:\nRequest from: ${req.ip}\nhttp info:\n` +
+      `method: ${req.method}\nhost: ${req.hostname}\npath: ${req.path}\n`;
 
+    if (req.params) message += `params: ${JSON.stringify(req.params)}\n`;
+    if (req.query) message += `query: ${JSON.stringify(req.query)}\n`;
+    var err = new Error(message);
 
+    err.status = 404;
+    next(err);
+  })
 
+  .use(function errorHandler(err, req, res, next) {
+    console.error(err);
 
-
-
-
-//for that asshole that's just trying to POST over and over again
-// app.post('/', (req, res, next) => {
-//   console.log (`that asshole that keeps POSTing did it again... IP: ${req.ip}`)
-//   res.status(403).send('fuck you')
-// })
-
-
-
-
-
-///////////////////////////////////
-//    ERROR RESPONSES
-///////////////////////////////////
-
-  .use (function routeNotFound(req, res, next){
-  let message = `Not Found:\nRequest from: ${req.ip}\nhttp info:\n`+
-      `method: ${req.method}\nhost: ${req.hostname}\npath: ${req.path}\n`
-
-  if (req.params) message += `params: ${JSON.stringify(req.params)}\n`
-  if (req.query) message += `query: ${JSON.stringify(req.query)}\n`
-  var err = new Error(message)
-
-  err.status = 404;
-  next(err)
-})
-
-  .use (function errorHandler(err, req, res, next){
-  console.error (err);
-
-  res.status(err.status || 500).send('Something went wrong!')
-})
-
-
+    res.status(err.status || 500).send('Something went wrong!');
+  });
 
 //
 // function errorHandler(err, req, res, next){
@@ -145,13 +110,33 @@ app.get('/gw2data/*', (req, res, next) => {
 //   res.status(err.status || 500).send('Something went wrong!')
 // }
 
+if (process.env.NODE_ENV === 'development') {
+  //this starts the server
+  httpServer.listen(80, () => {
+    console.log('http listening on port 80');
+  });
+}
 
+//this starts the server with greenlock to renew SSL certs
+else {
+  require('greenlock-express')
+    .init({
+      packageRoot: __dirname,
 
-//this starts the server
-httpServer.listen(80, () => {
-  console.log('http listening on port 80')
-})
+      // contact for security and critical bug notices
+      maintainerEmail: 'zach.swalberg@gmail.com',
 
-httpsServer.listen(443, () => {
-  console.log('https listening on port 443')
-})
+      // where to look for configuration
+      configDir: './greenlock.d',
+
+      // whether or not to run at cloudscale
+      cluster: false,
+    })
+    // Serves on 80 and 443
+    // Get's SSL certificates magically!
+    .serve(app);
+}
+
+// httpsServer.listen(443, () => {
+//   console.log('https listening on port 443')
+// })
